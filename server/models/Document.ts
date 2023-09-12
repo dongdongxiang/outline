a2ade1fa24d1fb30caa9e2a43d9c8869cd8c92fe
Apoorv Mishra <apoorvmishra101092@gmail.com>
2023-09-12 10:23:35 +0530

    fix: test

--- server/models/Document.ts
+++ server/models/Document.ts
@@ -31,6 +31,7 @@ import {
   Length as SimpleLength,
   IsNumeric,
   IsDate,
+  BelongsToMany,
 } from "sequelize-typescript";
 import isUUID from "validator/lib/isUUID";
 import type { NavigationNode } from "@shared/types";
@@ -41,6 +42,7 @@ import { DocumentValidation } from "@shared/validations";
 import { ValidationError } from "@server/errors";
 import Backlink from "./Backlink";
 import Collection from "./Collection";
+import DocumentUser from "./DocumentUser";
 import FileOperation from "./FileOperation";
 import Revision from "./Revision";
 import Star from "./Star";
@@ -153,6 +155,15 @@ type AdditionalFindOptions = {
       },
     ],
   },
+  withAllMemberships: {
+    include: [
+      {
+        model: DocumentUser,
+        as: "memberships",
+        required: false,
+      },
+    ],
+  },
   withViews: (userId: string) => {
     if (!userId) {
       return {};
@@ -171,6 +182,18 @@ type AdditionalFindOptions = {
       ],
     };
   },
+  withMembership: (userId: string) => ({
+    include: [
+      {
+        model: DocumentUser,
+        as: "memberships",
+        where: {
+          userId,
+        },
+        required: false,
+      },
+    ],
+  }),
 }))
 @Table({ tableName: "documents", modelName: "document" })
 @Fix
@@ -437,9 +460,15 @@ class Document extends ParanoidModel {
   @BelongsTo(() => Collection, "collectionId")
   collection: Collection | null | undefined;
 
+  @BelongsToMany(() => User, () => DocumentUser)
+  users: User[];
+
   @ForeignKey(() => Collection)
   @Column(DataType.UUID)
   collectionId?: string | null;
+
+  @HasMany(() => DocumentUser, "documentId")
+  memberships: DocumentUser[];
 
   @HasMany(() => Revision)
   revisions: Revision[];
